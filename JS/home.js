10a08e78ddabb3a199d1ffaf3943f776dc9e009b
andrupirvulescu <121255903+andrupirvulescu@users.noreply.github.com>
2023-05-01 15:11:24 +0300

    Update home.js

--- JS/home.js
+++ JS/home.js
@@ -12,54 +12,54 @@ var all = document.getElementById("button");
 all.addEventListener("click", function (event) {
   event.preventDefault();
   sessionStorage.setItem("filter", "all");
-  window.location.href = "category.html";
+  window.location.href = "https://andrupirvulescu.github.io/PetAdoptRomania/category.html";
 });
 
 var all = document.getElementById("button_sibiu");
 all.addEventListener("click", function (event) {
   event.preventDefault();
   sessionStorage.setItem("filter", "sibiu");
-  window.location.href = "category.html";
+  window.location.href = "https://andrupirvulescu.github.io/PetAdoptRomania/category.html";
 });
 
 var all = document.getElementById("button_alba");
 all.addEventListener("click", function (event) {
   event.preventDefault();
   sessionStorage.setItem("filter", "alba");
-  window.location.href = "category.html";
+  window.location.href = "https://andrupirvulescu.github.io/PetAdoptRomania/category.html";
 });
 
 var all = document.getElementById("button_hunedoara");
 all.addEventListener("click", function (event) {
   event.preventDefault();
   sessionStorage.setItem("filter", "hunedoara");
-  window.location.href = "category.html";
+  window.location.href = "https://andrupirvulescu.github.io/PetAdoptRomania/category.html";
 });
 
 var all = document.getElementById("button_valcea");
 all.addEventListener("click", function (event) {
   event.preventDefault();
   sessionStorage.setItem("filter", "valcea");
-  window.location.href = "category.html";
+  window.location.href = "https://andrupirvulescu.github.io/PetAdoptRomania/category.html";
 });
 
 var all = document.getElementById("button_brasov");
 all.addEventListener("click", function (event) {
   event.preventDefault();
   sessionStorage.setItem("filter", "brasov");
-  window.location.href = "category.html";
+  window.location.href = "https://andrupirvulescu.github.io/PetAdoptRomania/category.html";
 });
 
 var all = document.getElementById("button_cluj");
 all.addEventListener("click", function (event) {
   event.preventDefault();
   sessionStorage.setItem("filter", "cluj");
-  window.location.href = "category.html";
+  window.location.href = "https://andrupirvulescu.github.io/PetAdoptRomania/category.html";
 });
 
 var all = document.getElementById("button_mures");
 all.addEventListener("click", function (event) {
   event.preventDefault();
   sessionStorage.setItem("filter", "mures");
-  window.location.href = "category.html";
+  window.location.href = "https://andrupirvulescu.github.io/PetAdoptRomania/category.html";
 });
